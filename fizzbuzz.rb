e4c8f4cc5c00a4816306424c1779e2b4efc8ae17
Anthony Chang <awchang12@gmail.com>
2018-04-29 15:58:09 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,9 +1,10 @@
 def fizzbuzz(num)
-  if num % 3 && num % 5
+  if num % 3 == 0 && num % 5 == 0
     return "FizzBuzz"
-  elsif num % 3
-    return "Fizz"
-  elsif num % 5
+    elsif num % 3 == 0
+      return "Fizz"
+    end
+  elsif num % 5 == 0
     return "Buzz"
   else
     return nil
